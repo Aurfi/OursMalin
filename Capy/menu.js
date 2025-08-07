b19008b4ac424693acd19dc2489a01a8cc8191ad
Aurfi <aurelien.fiere@gmail.com>
2025-08-07 22:44:53 +0200

    Add Tap Capy Phaser mini game

--- Capy/menu.js
+++ Capy/menu.js
@@ -98,6 +98,14 @@
       image: 'assets/capybara_ninja_new.png'
     },
     {
+      id: 'tapcapy',
+      title: 'Tap Capy',
+      scoreKey: 'tapCapyHighScore',
+      page: 'tap_phaser.html',
+      category: 'arcade',
+      image: 'assets/capybara_running_clear.png'
+    },
+    {
       id: 'whack',
       title: 'Whack‑a‑Capy',
       scoreKey: 'capyWhackHighScore',
